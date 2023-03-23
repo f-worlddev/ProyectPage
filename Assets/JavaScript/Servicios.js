1f58c54cd66f897f883e0658e88453c177a87d69
Juanito3500 <juanchavezsaldana1@gmail.com>
2023-03-23 14:20:15 -0500

    Agregando descripciones a servicios

--- Assets/JavaScript/Servicios.js
+++ Assets/JavaScript/Servicios.js
@@ -1,26 +1,37 @@
-class Servicio{
 
-    constructor(
-        contenedorServicios,
-        nombreServicio,
-        rutaImagenRelativaAServicioJS,
-        texto,
-        colorLinearGradientFondoImagen = "to left top, rgb(48, 41, 255),rgb(63, 255, 223)",
-        colorTarjeta = "white",
-        tamanoNombreServicio = "1.3vw",
-        tamanoTexto = "1vw"
-    ){
 
-        let contenedorServicio = document.createElement('div');
+
+window.addEventListener('load',()=>{
+    
+
+    let contenedoresServicios = document.querySelectorAll('.Servicio-Contenedor');
+    let funcionesCambioAestilosOriginales = []
+
+    for(let i=0;i<contenedoresServicios.length;i++){
+        
+        contenedoresServicios[i].addEventListener('click',()=>{
+
+            let texto = document.querySelector(`.Servicio-Contenedor:nth-child(${i+1}) .Texto-Servicio`)
+            texto.style.display = "block"; 
+
+            funcionesCambioAestilosOriginales[i] = function(){
+                texto.style.display = "block";                       
+            }
+
+            contenedoresServicios[i].addEventListener('mouseover',funcionesCambioAestilosOriginales[i],true);
+
+            contenedoresServicios[i].addEventListener('mouseleave',()=>{
+                contenedoresServicios[i].style.minHeight = "50vh";
+                texto.style.display = "none"; 
+                contenedoresServicios[i].removeEventListener('mouseover',funcionesCambioAestilosOriginales[i],true);
+            })
+
+        },true)
         
 
     }
 
 
-
-}
-
-
-
+})
 
 
